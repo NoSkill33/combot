32ded1f67434b73cd6fad9f549aa9b5e3bff0a14
watermelonik <mieszkonowacki2@gmail.com>
2023-09-12 21:55:46 +0200

    Update bootstrap.py

--- bootstrap.py
+++ bootstrap.py
@@ -1,14 +1,1 @@
-import discord
-
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
-
-    async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
-
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = MyClient(intents=intents)
-client.run('MTE1MTI0MDEyMjYyNTQzMzcxMg.GzH02_.bOKuoeHeUNgJflXp_BQyPxv2i9Gip5KDkYQxOU')
+# nothing
